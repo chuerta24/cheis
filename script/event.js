ddf8b93acc24f6cc9291dab0ac658989e1b7707b
chuerta24 <huertachristopher69@gmail.com>
2026-03-04 17:08:41 +0000

    Wed, Mar 4, 2026, 9:08 AM -08:00

---
+++ script/event.js
@@ -0,0 +1,33 @@
+clickBox = document.querySelector('.click')
+spinBox = document.querySelector('.spin')
+moveBox = document.querySelector('.move')
+hoverBox = document.querySelector('.hover')
+dblClick = document.querySelector('.dblclick')
+
+colors = ['blue', 'green', 'purple', 'red']
+index = 0
+
+clickBox.addEventListener('click', () => {
+    clickBox.style.background = colors[index]
+    index = index + 1
+    if (index == colors.length){
+        index = 0
+    }
+})
+
+spinBox.addEventListener('click', () => {
+    spinBox.classList.toggle('start')
+})
+
+x = 0
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowRight') {
+        x = x + 10;
+        moveBox.style.transform = `translate(${x}px, ${y}px)`
+    }
+    if (event.key == 'ArrowUp') {
+        y = y - 10;
+        moveBox.style.transform = `translate(${x}px, ${y}px)`
+    }
+})
