52690460dcf0d73004ac429a04ae45e297855417
chuerta24 <huertachristopher69@gmail.com>
2026-04-13 16:02:14 +0000

    Mon, Apr 13, 2026, 9:02 AM -07:00

--- script/ball.js
+++ script/ball.js
@@ -1,10 +1,11 @@
 const ball = document.createElement('div')
+const ballRadius = 30
 
 createBall()
 function createBall() {
     document.body.appendChild(ball)
-    ball.style.height = "100px"
-    ball.style.width = "100px"
+    ball.style.height = `${2 * ballRadius}px`
+    ball.style.width = `${2 * ballRadius}px`
     ball.style.borderRadius = "50%"
     ball.style.backgroundColor = "purple"
     ball.style.position = "absolute"
