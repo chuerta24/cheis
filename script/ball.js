a8a7cf4ba480b406e5546de55b9b57b5be38055f
chuerta24 <huertachristopher69@gmail.com>
2026-04-13 16:00:34 +0000

    Mon, Apr 13, 2026, 9:00 AM -07:00

--- script/ball.js
+++ script/ball.js
@@ -1,9 +1,13 @@
 const ball = document.createElement('div')
 
+createBall()
 function createBall() {
     document.body.appendChild(ball)
     ball.style.height = "100px"
     ball.style.width = "100px"
     ball.style.borderRadius = "50%"
     ball.style.backgroundColor = "purple"
+    ball.style.position = "absolute"
+    ball.style.top = "30px"
+    ball.style.left = "30px"
 }
