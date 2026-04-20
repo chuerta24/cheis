4cade4d3f256ee5d4a767f0018d63a61e6570ea7
chuerta24 <huertachristopher69@gmail.com>
2026-04-20 01:40:49 +0000

    Sun, Apr 19, 2026, 6:40 PM -07:00

--- script/relax.js
+++ script/relax.js
@@ -1,27 +1,27 @@
-const container = document.getElementById('container')
-const text = document.getElementById('text')
+const container = document.getElementById('container');
+const text = document.getElementById('text');
 
-totalTime = 7500
-breathTime = (totalTime / 5) * 2
-holdTime = totalTime / 5
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
 
-breathAnimation()
+breathAnimation();
 
 function breathAnimation() {
-    text.innerText = 'Breathe In!'
-    container.classList.remove('shrink')
-    container.classList.add('grow')
+    text.innerText = 'Breathe In!';
+    container.classList.remove('shrink');
+    container.classList.add('grow');
 
     setTimeout(() => {
-        text.innerText = 'Hold'
+        text.innerText = 'Hold';
 
         setTimeout(() => {
-            text.innerText = 'Breathe Out!'
-            container.classList.remove('grow')
-            container.classList.add('shrink')
-        }, holdTime)
+            text.innerText = 'Breathe Out!';
+            container.classList.remove('grow');
+            container.classList.add('shrink');
+        }, holdTime);
 
-    }, breathTime)
+    }, breathTime);
 }
 
-setInterval(breathAnimation, totalTime)
+setInterval(breathAnimation, totalTime);
